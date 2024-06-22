79116c86fe9f384bcbae7f9d6fa5a55f2d6b8942
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-06-22 17:58:13 +0300

    changed formatting

--- SQL-files/staging.sql
+++ SQL-files/staging.sql
@@ -1,23 +1,24 @@
 USE DWH;
+
 DROP TABLE IF EXISTS CSV_STAGING;
 
 CREATE TABLE IF NOT EXISTS CSV_STAGING(
-invoice_id TEXT,
-branch_name TEXT,
-city TEXT,
-client_fname TEXT,
-client_lname TEXT,
-salesman_fname TEXT,
-salesman_lname TEXT,
-client_email TEXT,
-client_phone TEXT,
-product_name TEXT,
-product_line TEXT,
-product_price TEXT,
-amount TEXT,
-order_date TEXT,
-order_time TEXT,
-payment_method TEXT
+invoice_id          TEXT,
+branch_name         TEXT,
+city                TEXT,
+client_fname        TEXT,
+client_lname        TEXT,
+salesman_fname      TEXT,
+salesman_lname      TEXT,
+client_email        TEXT,
+client_phone        TEXT,
+product_name        TEXT,
+product_line        TEXT,
+product_price       TEXT,
+amount              TEXT,
+order_date          TEXT,
+order_time          TEXT,
+payment_method      TEXT
 );
 
 LOAD DATA INFILE %s
